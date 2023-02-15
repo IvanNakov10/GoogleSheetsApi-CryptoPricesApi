12521fb87283669259a95b7ee3eb76bb0a47ce8b
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-15 19:01:27 +0200

    +api

--- ImportingData.py
+++ ImportingData.py
@@ -1,4 +1,37 @@
 from openpyxl import Workbook
+import requests
+
+def getPriceOfCrypto():
+  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+  par={
+     'symbol' : 'BTC, ETH, GALA' ,
+     'convert' : 'USD'
+  }
+  headers = {
+     'Accepts': 'application/json',
+     'X-CMC_PRO_API_KEY': '634562be-a173-41d3-8c5d-e4229f52baea'
+  }
+
+  response = requests.get(url, headers=headers, params=par)
+
+  data = response.json()
+
+  btc = round(data['data']['BTC']['quote']['USD']['price'], 2)
+  eth = round(data['data']['ETH']['quote']['USD']['price'], 2)
+  gal = round(data['data']['GALA']['quote']['USD']['price'], 5)
+
+  btcpriceIvan = btc*0.001 
+  galaamount = gal*630.8
+  ethamount = eth*0.05227
+  hole_amount = round(btcpriceIvan+galaamount+ethamount, 2)
+
+
+
+
+
+
+
+
 
 wb = Workbook()
 ws = wb.active
